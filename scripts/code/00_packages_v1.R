7bad2c4e96f5d2e9c68e51f417405e1805076117
João da Silva <absilvamarques@gmail.com>
2025-03-29 12:40:26 -0300

    Criando Script separado para carregar pacotes necessários.

---
+++ scripts/code/00_packages_v1.R
@@ -0,0 +1,16 @@
+#######################################################
+########### Análise Exploratória do RENAEST ###########
+#######################################################
+### O objetivo principal deste projeto de pesquisa é a investigar os fatores de
+### risco e cenários críticos relacionados aos sinistros de trânsito no Brasil,
+### bem como reconhecer os estados/regiões que necessitam de maior foco para
+### reduzir os índices de vítimas no trânsito.
+
+######################################################
+### Script para carregar os pacotes necessários para a análise.
+
+library(readr)
+library(tidyverse)
+require(lubridate)
+library(ggplot2)
+library(dplyr)
